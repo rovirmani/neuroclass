51b1a7a89539c32e049d5c5f531edddbad4d3cd0
Siddharth Srinivasan <33235342+sidsrini@users.noreply.github.com>
2019-11-23 16:40:52 -0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,5 @@
 
 import matplotlib
-matplotlib.use("Agg")
 
 import keras
 model = ResNet.build(64, 128, 4, 4,
@@ -8,17 +7,10 @@ model = ResNet.build(64, 128, 4, 4,
 opt = SGD(lr=1e-2, momentum=0.9)
 model.compile(loss="binary_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
-j = model.fit_generator(
-	trainGen,
-	steps_per_epoch=totalTrain // BS,
-	validation_data=valGen,
-	validation_steps=totalVal // BS,
-	epochs=NUM_EPOCHS,
-	callbacks=callbacks)
-dicts = model.predict_generator(testGen,
-	steps=totalTest + 1)
+j = model.fit_generator(#fit gens here )
+dicts = model.predict_generator(#gensteps)
 
-dicts = np.argmax(predIdxs, axis=1)
+dicts = np.argmax(d, axis=1)
 print(classification_report(testGen.classes, predIdxs,
 	target_names=testGen.class_indices.keys()))
 
