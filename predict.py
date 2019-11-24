76473115c947247e2ce53dee7703c089ccec32ac
Siddharth Srinivasan <33235342+sidsrini@users.noreply.github.com>
2019-11-23 16:56:09 -0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,35 +1,7 @@
 import keras
 import cv2
-
-
-trainAug = ImageDataGenerator(
-	rescale=1 / 255.0,
-	rotation_range=20,
-	zoom_range=0.05,
-	width_shift_range=0.05,
-	height_shift_range=0.05,
-	shear_range=0.05,
-	horizontal_flip=True,
-	fill_mode="nearest")
-
-valAug = ImageDataGenerator(rescale=1 / 255.0)
-valAug = ImageDataGenerator(rescale=1 / 255.0)
-testGen = valAug.flow_from_directory(
-	config.TEST_PATH,
-	class_mode="categorical",
-	target_size=(64, 64),
-	color_mode="rgb",
-	shuffle=False,
-	batch_size=1)
+img = cv2.imread('img.png')
 model = load_model('classifier.h5')
-INIT_LR=1e-1
-opt = SGD(lr=INIT_LR, momentum=0.9)
-model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
 
-totalTest = len(list(paths.list_images(config.TEST_PATH)))
-print(totalTest)
-pred = model.predict_generator(testGen,
-	steps=(totalTest) + 1)
-
+pred = model.predict_generator(img)
 print(pred)
